356dd5808ad656e7b98ff154d5d7937aeef5e11d
R3AP3Redit <147928812+iotserver24@users.noreply.github.com>
2024-05-11 15:56:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+
+
+
+
+import logging
 from telegram import Update, Bot
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+
+# Configure logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
 bot = Bot(token='7188890855:AAGOR_3iZSjZqk3DSFiMfZrA1bRbvKsngYs')
@@ -19,6 +27,7 @@ def send_message_to_user(update: Update, context: CallbackContext) -> None:
 
     # Send the message to the user
     context.bot.send_message(chat_id=user_id, text=message_to_send)
+    logging.info(f"Message sent to user {username} (ID: {user_id})")
 
 def main() -> None:
     updater = Updater('7188890855:AAGOR_3iZSjZqk3DSFiMfZrA1bRbvKsngYs')
@@ -28,7 +37,8 @@ def main() -> None:
     dispatcher.add_handler(MessageHandler(Filters.text & Filters.regex(r'^/user @\w+'), send_message_to_user))
 
     updater.start_polling()
+    logging.info("Bot started polling.")
     updater.idle()
 
-if name == 'main':
+if __name__ == '__main__':
     main()
